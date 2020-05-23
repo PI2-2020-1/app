bd95c58825702c4ead24cffcb2135e64fc0d01c6
Thiago Pereira <thiagorpereira7@gmail.com>
2020-05-23 02:16:46 -0300

    Default styles

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import sensorService from '../../services/SensorService';
 import './App.css';
 
-const App = () => {
+const Home = () => {
   const [temperatures, setTemperatures] = useState(null);
 
   useEffect(() => {
@@ -48,4 +48,4 @@ const App = () => {
   );
 }
 
-export default App;
+export default Home;
---
+++ src/pages/_layouts/default/styles.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  height: 100%;
+  background: #d9d9f3;
+`;
